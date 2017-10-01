9ffd7f1c14eaee230d1478357d167ba70dc78b92
Riley Littlefield <rileylittlefield@ymail.com>
2017-09-30 21:06:17 -0400

    changes file names

--- frontend/scripts/components/matchmaking.component.ts
+++ frontend/scripts/components/matchmaking.component.ts
@@ -4,7 +4,7 @@ import { Message } from '@stomp/stompjs';
 import { StompService, StompState, StompConfig } from '@stomp/ng2-stompjs';
 import { Subscription } from 'rxjs/Subscription';
 
-import { StompServiceFacade } from '../stomp-module/services/stomp.service';
+import { StompServiceFacade } from '../stomp-module/services/stomp.service.facade';
 
 @Component({
   template: `
@@ -32,41 +32,8 @@ export class MatchmakingComponent {
 
   constructor(@Inject(StompServiceFacade) StompServiceFacade) {
     console.log("HELLO")
-
-    // let stompService:StompService = new StompService({
-    //   url: 'ws://localhost:8090/matchmaking/websocket',
-    //   headers: {
-    //     username: "user1"
-    //   },
-    //   heartbeat_in: 0,
-    //   heartbeat_out: 20000,
-    //   reconnect_delay: 5000,
-    //   debug: true
-    // });
-    //
-    // var subscription:Subscription = stompService.subscribe('/topic/matchmaking-stats')
-    // .map((message: Message) => {
-    //   return message.body;
-    // }).subscribe((messageBody: string) => {
-    //   console.log(`Received: ${messageBody}`);
-    // });
     debugger
 
-    // stompService.publish('/app/matchmaking', JSON.stringify({ roomName: "sup" }));
-
-
-
-    // this.rooms =  [
-    //   Room.fromPOJO({
-    //     name: "ROOM ONE",
-    //     maxNumberOfUsers: 3
-    //   }),
-    //
-    //   Room.fromPOJO({
-    //     name: "ROOM TWO",
-    //     maxNumberOfUsers: 3
-    //   })
-    // ];
   }
 
 }
--- frontend/scripts/stomp-module/stomp.module.ts
+++ frontend/scripts/stomp-module/stomp.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 // import { STOMP_CONFIG } from './constants/stomp.config';
 // import { StompService, StompConfig } from '@stomp/ng2-stompjs';
-import { StompServiceFacade } from './services/stomp.service';
+import { StompServiceFacade } from './services/stomp.service.facade';
 
 @NgModule({
   providers: [
