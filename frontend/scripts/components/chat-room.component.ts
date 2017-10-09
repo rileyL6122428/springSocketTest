1a7e204b0cf5857cf40caa69c1071f34f3fe7384
Riley Littlefield <rileylittlefield@ymail.com>
2017-10-08 22:16:45 -0400

    adds params subscription to chat component

--- frontend/scripts/components/chat-room.component.ts
+++ frontend/scripts/components/chat-room.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { Room } from '../domain/Room';
 
 @Component({
   template: `
     <p>THIS IS THE CHAT ROOM COMPONENT</p>
   `
 })
-export class ChatRoomComponent {
+export class ChatRoomComponent implements OnInit, OnDestroy {
+
+  private subscriptions: Array<Subscription>;
+  private room: Room;
+
+  constructor(
+    @Inject(ActivatedRoute) private route: ActivatedRoute
+  ) {
+    this.subscriptions = new Array<Subscription>();
+  }
+
+  ngOnInit(): void {
+    let paramsSubsciption = this.route.params.subscribe((params) => {
+      
+    });
+
+    this.subscriptions.push(paramsSubsciption);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription =>  subscription.unsubscribe());
+  }
 
 }
