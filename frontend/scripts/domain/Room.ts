0b7c098451805096bb930e979ac512e41aa23f1e
Riley Littlefield <rileylittlefield@ymail.com>
2017-10-11 22:33:01 -0400

    updates Room.ts with messages field

--- frontend/scripts/domain/Room.ts
+++ frontend/scripts/domain/Room.ts
@@ -1,3 +1,5 @@
+import { ChatRoomMessage } from './ChatRoomMessage';
+
 export class Room {
 
   public static fromPOJO(pojo: Object) {
@@ -6,14 +8,26 @@ export class Room {
     room.name = pojo['name'];
     room.maxNumberOfUsers = pojo['maxNumberOfUsers'];
     room.totalNumberOfUsers = pojo['totalNumberOfUsers'];
+    room.messages = Room.messagesFromPOJOList(pojo['messages']);
 
     return room;
+  }
+
+  private static messagesFromPOJOList(messagePOJOs: Array<Object>): Array<ChatRoomMessage> {
+    let messages = new Array<ChatRoomMessage>();
+
+    messagePOJOs.forEach((messagePOJO: Object) => {
+      messages.push(ChatRoomMessage.fromPOJO(messagePOJO));
+    });
+
+    return messages;
   }
 
   private name: string;
   private maxNumberOfUsers: number;
   private totalNumberOfUsers: number;
-  
+  private messages: Array<ChatRoomMessage>;
+
 
   public getName(): string {
     return this.name;
