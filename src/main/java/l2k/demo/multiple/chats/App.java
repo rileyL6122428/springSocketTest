f6ffba5a5d1aa3482b6737cf8de1f82e8051d16e
Riley Littlefield <rileylittlefield@ymail.com>
2017-10-19 23:28:46 -0400

    separates websocket config from main class

--- src/main/java/l2k/demo/multiple/chats/App.java
+++ src/main/java/l2k/demo/multiple/chats/App.java
@@ -1,44 +1,15 @@
 package l2k.demo.multiple.chats;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-import l2k.demo.multiple.chats.channelinterceptor.MyChannelInterceptor;
-import l2k.demo.multiple.chats.services.UserService;
 
 @SpringBootApplication
 @EnableWebSocketMessageBroker
-public class App extends AbstractWebSocketMessageBrokerConfigurer {
+public class App {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 	
-	@Autowired
-	private UserService userService;
-
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/matchmaking").setAllowedOrigins("*").withSockJS();
-	}
-	
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.setInterceptors(new MyChannelInterceptor(){{
-			setUserService(userService);
-		}});
-	}
-	
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {	
-		registry.enableSimpleBroker("/topic", "/queue");
-		registry.setApplicationDestinationPrefixes("/app", "/topic");
-	}
-
 }
---
+++ src/main/java/l2k/demo/multiple/chats/WebSocketConfiguration.java
@@ -0,0 +1,36 @@
+package l2k.demo.multiple.chats;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import l2k.demo.multiple.chats.channelinterceptor.MyChannelInterceptor;
+import l2k.demo.multiple.chats.services.UserService;
+
+@Configuration
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/matchmaking").setAllowedOrigins("*").withSockJS();
+	}
+	
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.setInterceptors(new MyChannelInterceptor(){{
+			setUserService(userService); //TODO Use Spring DI for this
+		}});
+	}
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {	
+		registry.enableSimpleBroker("/topic", "/queue");
+		registry.setApplicationDestinationPrefixes("/app", "/topic");
+	}
+}
