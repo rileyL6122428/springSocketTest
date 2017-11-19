6e9f4452c9a575584aca38089b51157f2f00afb7
Riley Littlefield <rileylittlefield@ymail.com>
2017-11-18 21:37:21 -0500

    rescopes matchmakingComponent spec

--- trivia-client/src/app/matchmaking/matchmaking.component.spec.ts
+++ trivia-client/src/app/matchmaking/matchmaking.component.spec.ts
@@ -1,8 +1,7 @@
-import { ComponentFixture, TestBed, inject, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { MatchmakingComponent } from './matchmaking.component';
 import { UserService } from '../services/user.service';
 import { User } from '../domain/user/user';
-import { By } from '@angular/platform-browser';
 import { stubableObservable, stubableSubscription } from '../test-utils/mocks';
 import { ServicesModule } from '../services/service.module';
 import { MatchmakingService } from '../services/matchmaking.service';
@@ -89,7 +88,7 @@ describe('MatchmakingComponent', () => {
   });
 
   describe("username display", () => {
-    it("shows the username of the user returned from the userService", async(() => {
+    it("exposes the user returned from the user service", async(() => {
       let user = new User({ name: "TEST_USER_NAME" });
       (userService.getUser as jasmine.Spy).and.returnValue(Observable.create(
         (observer: Observer<User>) => observer.next(user)
@@ -97,69 +96,25 @@ describe('MatchmakingComponent', () => {
 
       _initializeMatchmakingComponent();
 
-      let usernameElement = fixture.debugElement.query(By.css("#welcome-user")).nativeElement;
-      expect(usernameElement.innerText).toEqual(`Welcome ${user.name}`);
+      expect(matchmakingComponent.user).toBe(user);
     }));
   });
 
   describe("matchmaking stats display", () => {
     let matchmakingStats: MatchmakingStats;
+    beforeEach( () => matchmakingStats = new MatchmakingStats({}) );
 
-    beforeEach(() => {
-      matchmakingStats = new MatchmakingStats({
-        unplacedUserTotal: 2,
-        rooms: [
-          new Room({
-            name: "ROOM_ONE",
-            maxNumberOfUsers: 3,
-            users: new Map<string, User>()
-                    .set("tim", new User({ name: "tim" }))
-                    .set("tom", new User({ name: "tom" }))
-          }),
-          new Room({
-            name: "ROOM_TWO",
-            maxNumberOfUsers: 5,
-            users: new Map<string, User>()
-                    .set("tam", new User({ name: "tam" }))
-          })
-        ]
-      });
-    });
-
-    it("shows the number of unplaced users returned from the matchmakingService", async(() => {
+    it("exposes the matchmaking stats returned from ", async(() => {
         (matchmakingService.getMatchmakingStats as jasmine.Spy).and.returnValue(Observable.create(
           (observer: Observer<MatchmakingStats>) => observer.next(matchmakingStats)
         ));
 
         _initializeMatchmakingComponent();
 
-        let unplacedUserElement = fixture.debugElement.query(By.css("#unplaced-user-total")).nativeElement;
-        expect(unplacedUserElement.innerText).toEqual(`${matchmakingStats.unplacedUserTotal} unplaced users`);
+        expect(matchmakingComponent.matchmakingStats).toBe(matchmakingStats);
     }));
 
-    it("shows the name, number of users, and the max number of users for each room returned from the matchmakingService", async(() => {
-        (matchmakingService.getMatchmakingStats as jasmine.Spy).and.returnValue(Observable.create(
-          (observer: Observer<MatchmakingStats>) => observer.next(matchmakingStats)
-        ));
-
-        _initializeMatchmakingComponent();
-
-        let roomsListElement = fixture.debugElement.query(By.css("#rooms")).nativeElement;
-        expect(roomsListElement.children.length).toBe(2);
-
-        let roomOneListItem: HTMLElement = roomsListElement.children[0];
-        expect(roomOneListItem.innerHTML).toContain(`ROOM_ONE`);
-        expect(roomOneListItem.innerHTML).toContain(`2/3`);
-
-        let roomTwoElement: HTMLElement = roomsListElement.children[1];
-        expect(roomTwoElement.innerHTML).toContain(`ROOM_TWO`);
-        expect(roomTwoElement.innerHTML).toContain(`1/5`);
-    }));
-  });
-
-  describe("matchmaking stats stomp subscription", () => {
     it("exposes stats retrieved from the stomp subscription", async(() => {
-      let matchmakingStats: MatchmakingStats = new MatchmakingStats({});
       (matchmakingService.subscribeToMatchmaking as jasmine.Spy).and.returnValue(Observable.create(
         (observer: Observer<MatchmakingStats>) => observer.next(matchmakingStats)
       ));
