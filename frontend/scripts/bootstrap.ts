56bbb0676a496fa78e165e03db32c117f5274c1f
Riley Littlefield <rileylittlefield@ymail.com>
2017-11-21 22:17:27 -0500

    deletes first attempt at trying to setup angular 4

--- frontend/scripts/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { TriviaAppModule } from './trivia-app-module';
-
-platformBrowserDynamic().bootstrapModule(TriviaAppModule);
