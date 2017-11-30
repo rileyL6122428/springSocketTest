4f3f51cc8cb004500861c18df49c41bbd574dc57
Riley Littlefield <rileylittlefield@ymail.com>
2017-11-29 22:48:33 -0500

    adds first unit spec for Trivia Game

--- src/test/java/l2k/demo/multiple/chats/unit/game/TriviaGameTest.java
+++ src/test/java/l2k/demo/multiple/chats/unit/game/TriviaGameTest.java
@@ -2,13 +2,40 @@ package l2k.demo.multiple.chats.unit.game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 
+import l2k.demo.multiple.chats.game.QuestionRoll;
+import l2k.demo.multiple.chats.game.ScoreKeeper;
+import l2k.demo.multiple.chats.game.TriviaGame;
+import name.falgout.jeffrey.testing.junit5.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class TriviaGameTest {
+	
+	private TriviaGame game;
 
+	@Mock
+	private ScoreKeeper scoreKeeper;
+	@Mock
+	private QuestionRoll questionRoll;
+	
+	
+	@BeforeEach
+	public void setup() {
+		game = new TriviaGame(scoreKeeper, questionRoll);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void delegatesToTheQuestionRollWhenReportingTheQuetsionCount() {
+		when(questionRoll.getQuestionCount()).thenReturn(98);
+		int questionCount = game.getQuestionCount();
+		verify(questionRoll).getQuestionCount();
+		assertEquals(98, questionCount);
 	}
 
 }
