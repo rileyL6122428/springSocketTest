5e8b1917e59b99ff6f1290bb5ff5cac50e4ae5d3
Riley Littlefield <rileylittlefield@ymail.com>
2017-12-16 16:43:31 -0500

    adds unit spec for UserService#getUser

--- src/test/java/l2k/trivia/unit/server/services/UserServiceTest.java
+++ src/test/java/l2k/trivia/unit/server/services/UserServiceTest.java
@@ -1,7 +1,11 @@
 package l2k.trivia.unit.server.services;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,9 +22,6 @@ import l2k.trivia.server.domain.User;
 import l2k.trivia.server.services.NameGenerator;
 import l2k.trivia.server.services.UserService;
 import name.falgout.jeffrey.testing.junit5.MockitoExtension;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.not;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -66,6 +67,25 @@ class UserServiceTest {
 			UUID sessionId = UUID.randomUUID();
 			User user = userService.registerUser(sessionId);
 			assertThat(user.getSessionId(), not(equalTo(sessionId)));
+		}
+		
+	}
+	
+	@Nested
+	class GetUser {
+		
+		@Test
+		public void returnsNullWhenSessionIdOfUnregisteredUserIsProvided() {
+			UUID randomSessionId = UUID.randomUUID();
+			User user = userService.getUser(randomSessionId);
+			assertNull(user);
+		}
+		
+		@Test
+		public void returnsUserWhenSessionIDOfRegisteredUserProvided() {
+			User user = userService.registerUser(null);
+			User retrievedUser = userService.getUser(user.getSessionId());
+			assertEquals(user, retrievedUser);
 		}
 		
 	}
