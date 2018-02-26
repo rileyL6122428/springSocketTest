47f045ae93028689bd307fcc32fa7b90fb375687
Riley Littlefield <rileylittlefield@ymail.com>
2018-02-25 22:57:30 -0500

    cleans up UI

--- trivia-client/src/app/app.component.ts
+++ trivia-client/src/app/app.component.ts
@@ -8,19 +8,7 @@ import { SessionService } from './services/session/session.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <section id="app-header">
-      <h1>
-        <img class="site-logo" src="./assets/images/happy-face.png" />
-        <span class="header-text">Happy Place Trivia</span>
-      </h1>
-      <h3 id="welcome-user">
-        Your happy place name is: <span class="username">{{username}}</span>!
-      </h3>
-    </section>
-
-    <router-outlet></router-outlet>
-  `
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent extends SubscribingComponent implements OnInit {
 
@@ -37,18 +25,18 @@ export class AppComponent extends SubscribingComponent implements OnInit {
   ngOnInit(): void {
     this.addSubscriptions(
       this.sessionService.createSession().subscribe((sessionCreated: boolean) => {
-        if(sessionCreated) {
+        if (sessionCreated) {
           this.stompInitializer.setupStompService();
           this.userService.getUser().subscribe((user: User) => {
             this.user = user;
-          })
+          });
         }
       })
     );
   }
 
   get username(): string {
-    return this.user ? this.user.name : "...";
+    return this.user ? this.user.name : '...';
   }
 
 }
