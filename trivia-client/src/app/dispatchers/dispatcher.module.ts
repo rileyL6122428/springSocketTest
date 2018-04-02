de204897f28d7aa717f828984eff30d896ccc9a9
Riley Littlefield <rileylittlefield@ymail.com>
2018-04-01 23:10:27 -0400

    deletes dead code

--- trivia-client/src/app/dispatchers/dispatcher.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RoomMessageDispatcher } from './room-message.dispatcher';
-
-@NgModule({
-  providers: [ RoomMessageDispatcher ]
-})
-export class DispatcherModule {
-
-}
--- trivia-client/src/app/dispatchers/room-message.dispatcher.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { BaseDispatcher } from './base.dispatcher';
-
-@Injectable()
-export class RoomMessageDispatcher extends BaseDispatcher<any> {
-
-
-
-}
